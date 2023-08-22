348949ba437691bffa2c000a77ec76837761c964
weverson <weversonlek@gmail.com>
2023-08-22 07:29:09 -0300

    add css no CRUD

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Menu.css";
 import { Link } from "react-router-dom";
 
 export default function Menu() {
   return (
     <nav className="menu">
-      <ul>
+      <ul className="menu-ul">
         <li>
           <Link to="/">Home</Link>
         </li>
